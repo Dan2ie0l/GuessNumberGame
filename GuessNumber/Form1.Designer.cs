80aac1ede0b4abf37d8c29812d179b6d9ecb32af
Dan2ie0l <daniel.harutyunyan.20@gmail.com>
2021-02-18 02:37:58 +0400

    lessmore

--- GuessNumber/Form1.Designer.cs
+++ GuessNumber/Form1.Designer.cs
@@ -28,13 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnTry = new System.Windows.Forms.Button();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblRes2 = new System.Windows.Forms.Label();
+            this.btnPoqr = new System.Windows.Forms.Button();
+            this.btnHavasar = new System.Windows.Forms.Button();
+            this.lblEnd = new System.Windows.Forms.Label();
+            this.btnMec = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(363, 45);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(103, 32);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnTry
+            // 
+            this.btnTry.Location = new System.Drawing.Point(124, 266);
+            this.btnTry.Name = "btnTry";
+            this.btnTry.Size = new System.Drawing.Size(124, 55);
+            this.btnTry.TabIndex = 1;
+            this.btnTry.Text = "Try Again";
+            this.btnTry.UseVisualStyleBackColor = true;
+            this.btnTry.Click += new System.EventHandler(this.btnTry_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(124, 52);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(209, 20);
+            this.txtNumber.TabIndex = 2;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(145, 109);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 3;
+            // 
+            // lblRes2
+            // 
+            this.lblRes2.AutoSize = true;
+            this.lblRes2.Location = new System.Drawing.Point(145, 167);
+            this.lblRes2.Name = "lblRes2";
+            this.lblRes2.Size = new System.Drawing.Size(0, 13);
+            this.lblRes2.TabIndex = 4;
+            // 
+            // btnPoqr
+            // 
+            this.btnPoqr.Location = new System.Drawing.Point(349, 209);
+            this.btnPoqr.Name = "btnPoqr";
+            this.btnPoqr.Size = new System.Drawing.Size(103, 32);
+            this.btnPoqr.TabIndex = 5;
+            this.btnPoqr.Text = "Poqr";
+            this.btnPoqr.UseVisualStyleBackColor = true;
+            this.btnPoqr.Click += new System.EventHandler(this.btnPoqr_Click);
+            // 
+            // btnHavasar
+            // 
+            this.btnHavasar.Location = new System.Drawing.Point(349, 167);
+            this.btnHavasar.Name = "btnHavasar";
+            this.btnHavasar.Size = new System.Drawing.Size(103, 32);
+            this.btnHavasar.TabIndex = 6;
+            this.btnHavasar.Text = "Havasar";
+            this.btnHavasar.UseVisualStyleBackColor = true;
+            this.btnHavasar.Click += new System.EventHandler(this.btnHavasar_Click);
+            // 
+            // lblEnd
+            // 
+            this.lblEnd.AutoSize = true;
+            this.lblEnd.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnd.Location = new System.Drawing.Point(566, 138);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(0, 25);
+            this.lblEnd.TabIndex = 7;
+            // 
+            // btnMec
+            // 
+            this.btnMec.Location = new System.Drawing.Point(349, 129);
+            this.btnMec.Name = "btnMec";
+            this.btnMec.Size = new System.Drawing.Size(103, 32);
+            this.btnMec.TabIndex = 8;
+            this.btnMec.Text = "Mec";
+            this.btnMec.UseVisualStyleBackColor = true;
+            this.btnMec.Click += new System.EventHandler(this.btnMec_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMec);
+            this.Controls.Add(this.lblEnd);
+            this.Controls.Add(this.btnHavasar);
+            this.Controls.Add(this.btnPoqr);
+            this.Controls.Add(this.lblRes2);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.btnTry);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnTry;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblRes2;
+        private System.Windows.Forms.Button btnPoqr;
+        private System.Windows.Forms.Button btnHavasar;
+        private System.Windows.Forms.Label lblEnd;
+        private System.Windows.Forms.Button btnMec;
     }
 }
 
--- GuessNumber/Form1.cs
+++ GuessNumber/Form1.cs
@@ -12,9 +12,79 @@ namespace GuessNumber
 {
     public partial class Form1 : Form
     {
+        static Random rnd = new Random();
+        static int mtapahac = rnd.Next(1, 100);
+        static int mtapahac2 = 0;
+        int i = 0;
+        int j = 100;
+
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            
+            int number = int.Parse(txtNumber.Text);
+            if (number == mtapahac )
+            {
+                lblResult.Text = Convert.ToString("Gushakecir");
+            }
+            else if (number > mtapahac)
+            {
+                lblResult.Text = Convert.ToString("Porcir krkin, mec e");
+            }
+            else
+            {
+                lblResult.Text = Convert.ToString("Porcir krkin, poqr e");
+            }
+
+            lblRes2.Text = Convert.ToString(mtapahac2);
+
+        }
+
+        private void btnHavasar_Click(object sender, EventArgs e)
+        {
+            lblEnd.Text = Convert.ToString("You Win!");
+
+        }
+
+        private void btnMec_Click(object sender, EventArgs e)
+        {
+            
+            int i = this.i;
+            int j = this.j;
+           
+            
+               int mid = (i + j) / 2;
+               this.i = mid;
+            
+            mtapahac2 = mid;
+            lblRes2.Text = Convert.ToString(mtapahac2);
+
+        }
+
+        private void btnPoqr_Click(object sender, EventArgs e)
+        {
+            int i = this.i;
+            int j = this.j;
+            
+            int mid = (i + j) / 2;
+            this.j = mid;
+            mtapahac2 = mid;
+            lblRes2.Text = Convert.ToString(mtapahac2);
+        }
+
+        private void btnTry_Click(object sender, EventArgs e)
+        {
+            txtNumber.Text = Convert.ToString(" ");
+            lblRes2.Text = Convert.ToString(" ");
+            lblEnd.Text = Convert.ToString(" ");
+            lblResult.Text = Convert.ToString(" ");
+            this.i = 0;
+            this.j = 100;
         }
     }
 }
