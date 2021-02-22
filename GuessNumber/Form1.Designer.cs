a4ab5ff8dc44ec5960eace2c91f8c36f53ea8713
Dan2ie0l <daniel.harutyunyan.20@gmail.com>
2021-02-23 00:42:59 +0400

    binarysearch

--- GuessNumber/Form1.Designer.cs
+++ GuessNumber/Form1.Designer.cs
@@ -49,7 +49,7 @@
             // 
             this.btnCheck.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btnCheck.ForeColor = System.Drawing.Color.Blue;
-            this.btnCheck.Location = new System.Drawing.Point(110, 300);
+            this.btnCheck.Location = new System.Drawing.Point(44, 305);
             this.btnCheck.Name = "btnCheck";
             this.btnCheck.Size = new System.Drawing.Size(103, 32);
             this.btnCheck.TabIndex = 0;
@@ -62,7 +62,7 @@
             this.btnTry.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.btnTry.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btnTry.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.btnTry.Location = new System.Drawing.Point(255, 300);
+            this.btnTry.Location = new System.Drawing.Point(189, 305);
             this.btnTry.Name = "btnTry";
             this.btnTry.Size = new System.Drawing.Size(124, 55);
             this.btnTry.TabIndex = 1;
@@ -72,7 +72,7 @@
             // 
             // txtNumber
             // 
-            this.txtNumber.Location = new System.Drawing.Point(205, 56);
+            this.txtNumber.Location = new System.Drawing.Point(139, 61);
             this.txtNumber.Name = "txtNumber";
             this.txtNumber.Size = new System.Drawing.Size(209, 20);
             this.txtNumber.TabIndex = 2;
@@ -80,7 +80,7 @@
             // lblResult
             // 
             this.lblResult.AutoSize = true;
-            this.lblResult.Location = new System.Drawing.Point(177, 122);
+            this.lblResult.Location = new System.Drawing.Point(166, 127);
             this.lblResult.Name = "lblResult";
             this.lblResult.Size = new System.Drawing.Size(31, 13);
             this.lblResult.TabIndex = 3;
@@ -90,7 +90,7 @@
             // 
             this.lblRes2.AutoSize = true;
             this.lblRes2.BackColor = System.Drawing.Color.White;
-            this.lblRes2.Location = new System.Drawing.Point(177, 180);
+            this.lblRes2.Location = new System.Drawing.Point(166, 185);
             this.lblRes2.Name = "lblRes2";
             this.lblRes2.Size = new System.Drawing.Size(31, 13);
             this.lblRes2.TabIndex = 4;
@@ -101,7 +101,7 @@
             this.rbGreater.AutoSize = true;
             this.rbGreater.BackColor = System.Drawing.Color.Red;
             this.rbGreater.ForeColor = System.Drawing.Color.White;
-            this.rbGreater.Location = new System.Drawing.Point(366, 127);
+            this.rbGreater.Location = new System.Drawing.Point(300, 132);
             this.rbGreater.Name = "rbGreater";
             this.rbGreater.Size = new System.Drawing.Size(85, 17);
             this.rbGreater.TabIndex = 9;
@@ -113,7 +113,7 @@
             // 
             this.rbEqual.AutoSize = true;
             this.rbEqual.BackColor = System.Drawing.Color.Lime;
-            this.rbEqual.Location = new System.Drawing.Point(366, 153);
+            this.rbEqual.Location = new System.Drawing.Point(300, 158);
             this.rbEqual.Name = "rbEqual";
             this.rbEqual.Size = new System.Drawing.Size(60, 17);
             this.rbEqual.TabIndex = 10;
@@ -126,7 +126,7 @@
             this.rbLess.AutoSize = true;
             this.rbLess.BackColor = System.Drawing.Color.Blue;
             this.rbLess.ForeColor = System.Drawing.Color.White;
-            this.rbLess.Location = new System.Drawing.Point(366, 176);
+            this.rbLess.Location = new System.Drawing.Point(300, 181);
             this.rbLess.Name = "rbLess";
             this.rbLess.Size = new System.Drawing.Size(72, 17);
             this.rbLess.TabIndex = 11;
@@ -137,7 +137,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 122);
+            this.label1.Location = new System.Drawing.Point(37, 127);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(85, 13);
             this.label1.TabIndex = 12;
@@ -146,7 +146,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(39, 180);
+            this.label2.Location = new System.Drawing.Point(41, 185);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(81, 13);
             this.label2.TabIndex = 13;
@@ -157,7 +157,7 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Gainsboro;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(478, 127);
+            this.label3.Location = new System.Drawing.Point(432, 132);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(336, 48);
             this.label3.TabIndex = 14;
@@ -169,17 +169,17 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(15, 56);
+            this.label4.Location = new System.Drawing.Point(54, 62);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(160, 15);
+            this.label4.Size = new System.Drawing.Size(68, 15);
             this.label4.TabIndex = 15;
-            this.label4.Text = "Code remembered number:";
+            this.label4.Text = "AI Number:";
             // 
             // lblAttemptsCount
             // 
             this.lblAttemptsCount.AutoSize = true;
             this.lblAttemptsCount.BackColor = System.Drawing.Color.White;
-            this.lblAttemptsCount.Location = new System.Drawing.Point(252, 234);
+            this.lblAttemptsCount.Location = new System.Drawing.Point(166, 253);
             this.lblAttemptsCount.Name = "lblAttemptsCount";
             this.lblAttemptsCount.Size = new System.Drawing.Size(31, 13);
             this.lblAttemptsCount.TabIndex = 16;
@@ -189,7 +189,7 @@
             // 
             this.lblWinGameOverIWin.AutoSize = true;
             this.lblWinGameOverIWin.BackColor = System.Drawing.Color.White;
-            this.lblWinGameOverIWin.Location = new System.Drawing.Point(609, 271);
+            this.lblWinGameOverIWin.Location = new System.Drawing.Point(546, 271);
             this.lblWinGameOverIWin.Name = "lblWinGameOverIWin";
             this.lblWinGameOverIWin.Size = new System.Drawing.Size(31, 13);
             this.lblWinGameOverIWin.TabIndex = 17;
--- GuessNumber/Form1.cs
+++ GuessNumber/Form1.cs
@@ -14,11 +14,11 @@ namespace GuessNumber
     public partial class Form1 : Form
     {
         public Random NumberGenerator = new Random();
-        public int rememberedNum = 0;
-        public int rememberedNumOfPlayer = 0;
+        public int AINum = 0;
+        public int NumOfPlayer = 50;
         public int attemptsCount = 10;
-        public int i = 1;
-        public int j = 100;
+        public int min = 1;
+        public int max = 100;
 
 
         public Form1()
@@ -29,43 +29,29 @@ namespace GuessNumber
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            rememberedNum = NumberGenerator.Next(1, 101);
+            AINum = NumberGenerator.Next(1, 101);
             lblResult.Text = "";
             lblRes2.Text = "";
             lblWinGameOverIWin.Text = "";
-            lblAttemptsCount.Text = attemptsCount + " attempts" + " left";
+           
 
         }
 
         private void btnCheck_Click(object sender, EventArgs e)
         {
+            lblAttemptsCount.Text = attemptsCount + " attempts" + " left";
+            attemptsCount--;
+            AINumberGuessing();
 
-            int number = int.Parse(txtNumber.Text);
-            if (number == rememberedNum)
+            PlayersNumberGuessing();
+
+            lblRes2.Text = Convert.ToString(NumOfPlayer);
+
+            if (attemptsCount <= 0)
             {
-                lblResult.ForeColor = Color.Green;
-                lblResult.Text = Convert.ToString("You guessed it");
-
+                lblAttemptsCount.Enabled = false;
+                lblWinGameOverIWin.Text = Convert.ToString("Game Over!");
             }
-            else if (number > rememberedNum)
-            {
-                lblResult.ForeColor = Color.Red;
-                lblResult.Text = Convert.ToString("Try again, it's greater");
-            }
-            else
-            {
-                lblResult.ForeColor = Color.Blue;
-                lblResult.Text = Convert.ToString("Try again, it's less");
-            }
-            if (rbEqual.Checked)
-            {
-                lblRes2.Text = "I Win!";
-                lblWinGameOverIWin.Text = "Game Over";
-            }
-            
-
-
-
         }
 
 
@@ -77,7 +63,47 @@ namespace GuessNumber
         }
 
 
+        void AINumberGuessing()
+        {
+            int number = int.Parse(txtNumber.Text);
+            if (number == AINum)
+            {
+                lblResult.ForeColor = Color.Green;
+                lblResult.Text = Convert.ToString("You guessed it");
 
+            }
+            else if (number > AINum)
+            {
+                lblResult.ForeColor = Color.Red;
+                lblResult.Text = Convert.ToString("Try again, it's greater");
+            }
+            else
+            {
+                lblResult.ForeColor = Color.Blue;
+                lblResult.Text = Convert.ToString("Try again, it's less");
+            }
+        }
+
+        void PlayersNumberGuessing()
+        {
+            if (rbEqual.Checked)
+            {
+                
+                lblWinGameOverIWin.Text = "Game Over, I Won!!";
+            }
+            else if (rbGreater.Checked)
+            {
+                min = NumOfPlayer;
+                NumOfPlayer = (min + max) / 2;
+            }
+            else if (rbLess.Checked)
+            {
+                max = NumOfPlayer;
+                NumOfPlayer = (min + max) / 2;
+            }
+           
+
+        }
 
     }
 }
